893e633924f0629999420bcca6f874d6b76bcfde
Mariia <mariiapopovych@gmail.com>
2019-05-09 21:56:54 -0700

    add

--- Whiteboarding.cs
+++ Whiteboarding.cs
@@ -3,36 +3,30 @@ using System.Collections.Generic;
 
 public class Program
 {
- static void Main(int max)
+public static void Main()
+{
+	int max = 10;// 0 1 1 2 3 5 8
+
+	List<int> list = new List<int>();
+
+	for (int i = 1; i < max; i++)
 	{
-
-
-		List<int> list = new List<int>();
-
-		for (int i =1; i< max; i++)
+		bool IsPrime = true;
+		for (int j = 2; j <= i/2; j++)
 		{
-
-				bool IsPrime=true;
-		for (int j=2; j < i/2; j++)
-		{
-  			if (i%j == 0)
-  		{
-    		IsPrime = false;
-  		}
-	}
+			if (i%j == 0)
+			{
+				IsPrime = false;
+			}
+		}
 		if(IsPrime)
 		{
-  	 		list.Add(i);
-
-}
-
-}
-		foreach ( var mn in list)
-
-		{
-		Console.WriteLine(mn);
+			list.Add(i);
 		}
-
-
 	}
+	foreach ( var mn in list)
+	{
+		Console.WriteLine(mn);
+	}
+}
 }
