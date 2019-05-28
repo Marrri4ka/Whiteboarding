d2fdb8280a2c2a55d522aa2f723d8af2c087fe36
Student <example@example.com>
2019-05-28 16:35:59 -0700

    start solving problem

---
+++ Return.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+public class Return
+{
+  public static void Main()
+  {
+    Console.WriteLine ("Please enter a number!");
+    int userInput = int.Parse(Console.ReadLine());
+
+  for (int i = 0; i < userInput; i++)
+  {
+
+
+      Console.WriteLine (i);
+    }
+  }
+
+
+  }
