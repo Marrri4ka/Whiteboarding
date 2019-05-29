371d3140d3a33a3a10e34db4e23ffe8134c62d25
Mariia <mariiapopovych@gmail.com>
2019-05-28 17:35:42 -0700

    add

--- Return.cs
+++ Return.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 
 public class Return
 {
-  public static void Main()
-  {
-    Console.WriteLine ("Please enter a number!");
-    int userInput = int.Parse(Console.ReadLine());
+public static void Main()
+{
+	Console.WriteLine ("Please enter a number!");
+	int userInput = int.Parse(Console.ReadLine());
 
-  for (int i = 0; i < userInput; i++)
-  {
-
-
-      Console.WriteLine (i);
-    }
-  }
+	for (int i = 0; i < userInput; i++)
+	{
 
 
-  }
+		if(i%2 == 0)
+		{
+			Console.WriteLine ("+");
+		}
+		else
+		{
+			Console.WriteLine("-");
+		}
+	}
+}
+
+
+}
